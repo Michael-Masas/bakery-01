5a4ae9e86df0c0888f48ef553949a6b3f8a9e10c
root <root@centos7.local>
2020-11-05 10:48:17 +0200

    commenting jenkins config for JSasC

--- Michael-Masas/ex3/jenkins_container_service.sh
+++ Michael-Masas/ex3/jenkins_container_service.sh
@@ -24,42 +24,42 @@ systemctl enable docker
 echo "starting docker" 
 systemctl start docker 
 
-echo "add jenkins configurations"
-sudo groupadd --system jenkins
-sudo useradd -s /sbin/nologin --system -g jenkins jenkins
-sudo usermod -aG docker jenkins
-docker pull jenkins/jenkins:lts
-sudo mkdir /var/jenkins
-sudo chown -R 1000:1000 /var/jenkins
-sudo touch /etc/systemd/system/jenkins-docker.service
-sudo echo "[Unit]
-Description=Jenkins Server
-Documentation=https://jenkins.io/doc/
-After=docker.service
-Requires=docker.service
-
-[Service]
-Type=simple
-User=jenkins
-Group=jenkins
-TimeoutStartSec=0
-Restart=on-failure
-RestartSec=30s
-ExecStartPre=-/usr/bin/docker kill jenkins-server
-ExecStartPre=-/usr/bin/docker rm jenkins-server
-ExecStartPre=/usr/bin/docker pull jenkins/jenkins:lts
-ExecStart=/usr/bin/docker run \
-          --name jenkins-server \
-          --publish 8080:8080 \
-          --publish 50000:50000 \
-          --volume /var/jenkins:/var/jenkins_home \
-          jenkins/jenkins:lts
-SyslogIdentifier=jenkins
-ExecStop=/usr/bin/docker stop jenkins-server
-
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/jenkins-docker.service
-
-systemctl daemon-reload
-systemctl start jenkins-docker
-
+#echo "add jenkins configurations"
+#sudo groupadd --system jenkins
+#sudo useradd -s /sbin/nologin --system -g jenkins jenkins
+#sudo usermod -aG docker jenkins
+#docker pull jenkins/jenkins:lts
+#sudo mkdir /var/jenkins
+#sudo chown -R 1000:1000 /var/jenkins
+#sudo touch /etc/systemd/system/jenkins-docker.service
+#sudo echo "[Unit]
+#Description=Jenkins Server
+#Documentation=https://jenkins.io/doc/
+#After=docker.service
+#Requires=docker.service
+#
+#[Service]
+#Type=simple
+#User=jenkins
+#Group=jenkins
+#TimeoutStartSec=0
+#Restart=on-failure
+#RestartSec=30s
+#ExecStartPre=-/usr/bin/docker kill jenkins-server
+#ExecStartPre=-/usr/bin/docker rm jenkins-server
+#ExecStartPre=/usr/bin/docker pull jenkins/jenkins:lts
+#ExecStart=/usr/bin/docker run \
+#          --name jenkins-server \
+#          --publish 8080:8080 \
+#          --publish 50000:50000 \
+#          --volume /var/jenkins:/var/jenkins_home \
+#          jenkins/jenkins:lts
+#SyslogIdentifier=jenkins
+#ExecStop=/usr/bin/docker stop jenkins-server
+#
+#[Install]
+#WantedBy=multi-user.target" > /etc/systemd/system/jenkins-docker.service
+#
+#systemctl daemon-reload
+#systemctl start jenkins-docker
+#
