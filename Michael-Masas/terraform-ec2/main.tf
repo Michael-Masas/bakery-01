ca453592fdcf53deb06db39e4aad2def2240bfe2
Michael Masas <hot.delek@gmail.com>
2020-12-26 22:18:54 +0200

    fixing creds

--- Michael-Masas/terraform-ec2/main.tf
+++ Michael-Masas/terraform-ec2/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = var.region
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
+  shared_credentials_file = "/root/.aws/credentials"
 } 
 
 resource "aws_vpc" "bakery_vpc" {
